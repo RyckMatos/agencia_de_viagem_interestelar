edc76ff2281904720cd03236fb8e1a2593643672
Henrique Matos <henriquematosteixeira@outlook.com>
2022-10-15 12:38:42 -0300

    Bugfix: Correcao imagem

--- app/Controllers/CadastroController.php
+++ app/Controllers/CadastroController.php
@@ -31,29 +31,16 @@
                     'estado' => $_POST['estado'],
                     'pais' => $_POST['pais'],
                 );
-                // $_SESSION['nome'] = $nome;
-                // $_SESSION['sobrenome'] = $sobrenome;
-                // $_SESSION['nascimento'] = $nascimento;
-                // $_SESSION['telefone'] = $telefone;
-                // $_SESSION['genero'] = $genero;
-                // $_SESSION['email'] = $email;
-                // $_SESSION['endereco'] = $endereco;
-                // $_SESSION['cidade'] = $cidade;
-                // $_SESSION['estado'] = $estado;
-                // $_SESSION['pais'] = $pais;
         
                 if($_POST['senha'] == $_POST['senha-confirma']) {
                     $senha = md5($_POST['senha']);
                 } else {
-                    // $_SESSION['msg_cadastro'] = 'Senhas incompatíveis';
                     $msg_cadastro = 'Senhas incompatíveis';
                     $this->view->render('cadastro', array('link_css_base' => 'styleLogin.css', 'link_css' => 'cadastro.css', 'msg_cadastro' => $msg_cadastro, 'registro' => $registro));
-                    // header('Location: /agencia_de_viagem_interestelar/cadastrar');
-                    // die();
                 }
+
                 $created_at = date('Y-m-d H:i:s');
                 $updated_at = date('Y-m-d H:i:s');
-        
                 
                 $VerificarUsuario = CadastroModel::verificaEmail($registro['email']);
                 // var_dump($VerificarUsuario);
@@ -65,23 +52,16 @@
                     $sql = \MySql::conectar();
                     $token = md5(uniqid());
                     $_SESSION['token'] = $token;
-                    $queryInsert = $sql->prepare("INSERT INTO `users` VALUES (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-                    $queryInsert->execute(array($token, $registro['nome'], $registro['sobrenome'], $registro['email'], $registro['telefone'], $registro['nascimento'], $registro['genero'], $registro['endereco'], $registro['cidade'], $registro['estado'], $registro['pais'], $senha, $created_at, $updated_at));
-        
-                    // $querySelector = $sql->prepare("SELECT * FROM users WHERE email=?");
-                    // $querySelector->execute(array($email));
-                    // $user = $queryInsert->fetchAll();
-                    // $_SESSION['user'] = $info;
-        
-                    // $this->view->render('home', array('link_css_base' => 'style.css', 'link_css' => 'home.css', 'user' => $user));
+                    $queryInsert = $sql->prepare("INSERT INTO `users` VALUES (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                    $queryInsert->execute(array(1, $token, $registro['nome'], $registro['sobrenome'], $registro['email'], $registro['telefone'], $registro['nascimento'], $registro['genero'], $registro['endereco'], $registro['cidade'], $registro['estado'], $registro['pais'], $senha, $created_at, $updated_at));
+
                     header('Location: /agencia_de_viagem_interestelar/');
                     die();
                 } else {
                     $_SESSION['msg_login'] = 'Email ja cadastrado!';
-                    // $msg_login = 'Email ja cadastrado!';
-                    // $this->view->render('login', array('link_css_base' => 'styleLogin.css', 'link_css' => 'login.css', 'msg_login' => $msg_login));
+
                     header('Location: /agencia_de_viagem_interestelar/login');
-                    // die();
+                    die();
                 }
             }
         }
--- database/create_users_table.php
+++ database/create_users_table.php
@@ -1,23 +1,20 @@
 <?php
-    // session_start();
-
     $conn = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die("Falha na conexão");
     $table = 'users';
     $result = mysqli_query($conn, "SHOW TABLES LIKE '$table'");
     $tableExists = $result && $result->num_rows > 0;
 
     if ($tableExists) {
-        // $_SESSION['banco'] = true;
-        // $_SESSION['msgBanco'] = 'Banco ja existente!';
+
     } else {
         $sql = "CREATE TABLE users (
             id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            `status` VARCHAR(255) NOT NULL COMMENT '0 = desativado, 1 = ativado, 2 = admin',
+            `status` INT NOT NULL COMMENT '0 = desativado, 1 = ativado, 2 = admin',
             token VARCHAR(255) NOT NULL UNIQUE,
             nome VARCHAR(255) NOT NULL,
             sobrenome VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL UNIQUE,
-            telefone varchar(20) NOT NULL,
+            telefone VARCHAR(20) NOT NULL,
             data_de_nascimento DATE NOT NULL,
             genero VARCHAR(10) NOT NULL,
             endereco VARCHAR(255) NOT NULL,
@@ -31,10 +28,6 @@
         $conn->query($sql);
 
         $conn->close();
-        // $_SESSION['banco'] = true;
-        // $_SESSION['msgBanco'] = 'Banco criado com sucesso!';
     }
-    
-    // header("Location: /agencia_de_viagem_interestelar/");
     
 ?>
